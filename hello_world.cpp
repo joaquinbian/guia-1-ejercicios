fd7cb1931855a7cfdba759d809c6c3e2192f1784
joaquinbian <joacobian21@gmail.com>
2024-03-20 23:36:35 -0300

    hola mundo y suma

---
+++ hello_world.cpp
@@ -0,0 +1,23 @@
+//importamos libreria para hacer print e ingresar info 
+#include <iostream>
+
+//usamos el namespace
+using namespace std;
+
+int main(){
+    int num1, num2, res;
+
+    cout << "Hola mundo";
+    
+    cout << "Ingresa el primer numero ";
+    cin >> num1; 
+
+    cout << "Ingresa el segundo numero ";
+    cin >> num2;
+
+    res = num1 + num2;
+
+    cout << res;
+
+    return 0;
+}
