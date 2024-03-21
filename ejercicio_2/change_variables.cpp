09cfbb837f1ba95bec84c7a374fe4e747fa9a355
joaquinbian <joacobian21@gmail.com>
2024-03-21 00:17:16 -0300

    ejercicio 2

---
+++ ejercicio_2/change_variables.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b, aux;
+
+    cout << "Ingrese un numero" << endl;
+    cin >> a;
+
+    cout << endl;
+    
+    cout << "Ingreseo otro numero" << endl;
+    cin >> b;
+
+    cout << endl;
+    
+    aux = a;
+    a = b;
+    b = aux;
+
+    cout << "El primer numero ingresado es ";
+    cout << a;
+    
+    cout << endl;
+
+
+    cout << "El segundo numero ingresado es ";
+    cout << b ;
+
+
+
+
+
+
+
+
+    cout << endl;
+    return 0;
+}
