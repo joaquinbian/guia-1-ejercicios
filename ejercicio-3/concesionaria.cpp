36727ad351708c2351706cdb340b129929aea128
joaquinbian <joacobian21@gmail.com>
2024-03-21 00:36:23 -0300

    ejercicio 3

--- ejercicio-3/concesionaria.cpp
+++ ejercicio-3/concesionaria.cpp
@@ -6,5 +6,24 @@ using namespace std;
 
 int main(){
  
+    int SALARY = 5000;
+    
+    int PLUS = 700;
+
+    int quantity, total_salary;
+
+    cout << "Ingrese la cantidad de autos que ha vendido " << endl;
+
+    cin >> quantity;
+
+    cout << endl;
+
+    total_salary = SALARY + (PLUS * quantity);
+
+
+    cout << "El salario del mes es de " << total_salary;
+    
+    cout << endl;
+
     return 0;
 }
